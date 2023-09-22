de0882c00dcd2e6f77808501ec2c09192a23efa6
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2023-09-22 23:33:09 +0600

    Initial commit

---
+++ src/main/java/com/arifhoque/main/controller/MainController.java
@@ -0,0 +1,4 @@
+package com.arifhoque.main.controller;
+
+public class MainController {
+}
---
+++ src/main/java/com/arifhoque/main/model/Candidate.java
@@ -0,0 +1,16 @@
+package com.arifhoque.main.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Candidate {
+    private Integer id;
+    private String name;
+    private String batch;
+}
